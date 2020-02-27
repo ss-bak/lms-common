4165c5e67596302585e6700452e953facd4693b6
Krissada Jindanupajit <krissada.jindanupajit@smoothstack.com>
2020-02-27 14:43:34 -0500

    Return type

--- repository/CopiesCommonRepository.java
+++ repository/CopiesCommonRepository.java
@@ -14,17 +14,41 @@ public interface CopiesCommonRepository extends JpaRepository<Copies, Long> {
     default Optional<Copies> findById(Long bookId, Long branchId) {
         return findByBook_BookIdAndBranch_BranchId(bookId, branchId);
     }
-
     Optional<Copies> findByBook_BookIdAndBranch_BranchId(Long bookId, Long branchId);
 
+
+    default boolean existsBy(Book book) {
+        return existsByBook(book);
+    }
     boolean existsByBook(Book book);
+    default boolean existsBy(Branch branch) {
+        return existsByBranch(branch);
+    }
     boolean existsByBranch(Branch branch);
+
 
     void deleteAllByBook(Book book);
 
 
-
+    default List<Copies> findAllBy(Branch branch, int minimumNoOfCopies) {
+        return findAllByBranchAndCopiesAmountGreaterThanEqual(branch,minimumNoOfCopies);
+    }
+    default List<Copies> findAllBy(Branch branch) {
+        return findAllBy(branch,0);
+    }
+    default List<Copies> findAvailableBy(Branch branch) {
+        return findAllBy(branch,1);
+    }
     List<Copies> findAllByBranchAndCopiesAmountGreaterThanEqual(Branch branch, int minimumNoOfCopies);
 
+    default Optional<Copies> findAllBy(Book book, Branch branch, int minimumNoOfCopies) {
+        return findAllByBookAndBranchAndCopiesAmountGreaterThanEqual(book, branch,minimumNoOfCopies);
+    }
+    default Optional<Copies> findAllBy(Book book, Branch branch) {
+        return findAllBy(book, branch,0);
+    }
+    default Optional<Copies> findAvailableBy(Book book, Branch branch) {
+        return findAllBy(book, branch,1);
+    }
     Optional<Copies> findAllByBookAndBranchAndCopiesAmountGreaterThanEqual(Book book, Branch branch, int minimumNoOfCopies);
 }
